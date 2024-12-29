795abfd09861a2fb27002089f52acbd66d401606
yzwou <yzwou@qq.com>
2024-12-29 15:22:08 +0800

    对其他添加了网址 修改了js文件

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// script.js
-
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const fileUrl = 'path_to_your_file/your_file_name.ext'; // 替换为你文件的路径
-    const fileName = 'your_file_name.ext';  // 替换为你文件的名称
-
-    // 创建一个隐藏的链接
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = fileName;
-
-    // 触发下载
-    link.click();
-
-    // 显示下载状态
-    document.getElementById('statusMessage').textContent = '下载开始...';
+document.getElementById('ys').addEventListener('click', function() {
+    window.alert('原神启动!');
 });
 
 function redirectTo(url) {
