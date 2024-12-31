40b6b473fb113bbac4bf18c310b35a1e938702c7
yzwou <yzwou@qq.com>
2024-12-31 21:52:01 +0800

    一些新东西

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ document.getElementById('ys').addEventListener('click', function() {
     window.alert('原神启动！');
     window.location.href = 'https://ys.mihoyo.com/';
 });
-document.querySelector('.b_java').addEventListener('click', function() {
+document.querySelector('.d_java').addEventListener('click', function() {
     window.alert('将进入JDK官网下载Java，下载速度可能较慢');
     window.location.href = 'https://www.oracle.com/cn/java/technologies/downloads/#jdk20-windows';
 });
-document.querySelector('.b_pcl').addEventListener('click', function() {
+document.querySelector('.pcl').addEventListener('click', function() {
     window.alert('建议使用PCL2而不是PCL 1');
     window.location.href = 'https://wwre.lanzouq.com/iEpJ02jbjd8d';
 });
-document.querySelector('.b_pcl2').addEventListener('click', function() {
+document.querySelector('.pcl2').addEventListener('click', function() {
     window.alert('访问密码：pcl2');
     window.location.href = 'https://ltcat.lanzouv.com/b0aj6gsid';
 });
-document.querySelector('.b_hmcl').addEventListener('click', function() {
+document.querySelector('.hmcl').addEventListener('click', function() {
     window.alert('访问密码：hmcl');
     window.location.href = 'https://wwre.lanzouq.com/iEpJ02jbjd8d';
 });
